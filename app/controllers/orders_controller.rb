c127ae9891dc6272b05a111d79feb971734ca77f
Timothy Meixell <misternu@gmail.com>
2018-02-07 20:47:34 -0600

    Add Stripe order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 # Create orders and respond to requests for shipping options
 class OrdersController < ApplicationController
   def create
+    begin
+      p StripeService.new.create_order
+    rescue Stripe::StripeError => e
+      p e
+    end
     redirect_to '/orders'
   end
 end
---
+++ app/services/stripe_service.rb
@@ -0,0 +1,35 @@
+# Service for dealing with stripe calls
+class StripeService
+  def create_order
+    Stripe::Order.create(
+      currency: 'usd',
+      customer: 'cus_CHdv4NciT0Y1MA',
+      items: fake_items,
+      :shipping => {
+        :name => 'Joe Public',
+        :address => {
+          :line1 => '222 W Merchandise Mart Plaza',
+          :city => 'Chicago',
+          :state => 'IL',
+          :country => 'US',
+          :postal_code => '60654'
+        }
+      }
+    )
+  end
+
+  def fake_items
+    [
+      {
+        type: 'sku',
+        parent: 'sku_BvqnHKG3smcasm',
+        quantity: 1
+      },
+      {
+        type: 'sku',
+        parent: 'sku_Bvqn9Foea5GIdC',
+        quantity: 1
+      }
+    ]
+  end
+end
